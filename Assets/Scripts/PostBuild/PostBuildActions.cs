19bf1b91e02fdaf4aa69f9ae9aceae211a394a57
alexanian <alexanyanyuka@gmail.com>
2022-10-03 10:19:31 +0200

    Minor improvement.

--- Assets/Scripts/PostBuild/PostBuildActions.cs
+++ Assets/Scripts/PostBuild/PostBuildActions.cs
@@ -17,18 +17,32 @@ namespace PostBuild
 
         private static void ConductPostBuildActions(BuildTarget target, string pathToBuiltProject) 
         {
-            if (target == BuildTarget.iOS) {
-       
-                string plistPath = pathToBuiltProject + "/Info.plist";
-                PlistDocument plist = new PlistDocument();
-                plist.ReadFromString(File.ReadAllText(plistPath));
-       
-                PlistElementDict rootDict = plist.root;
-                
-                rootDict.SetBoolean ("ITSAppUsesNonExemptEncryption", false);
-       
-                File.WriteAllText(plistPath, plist.WriteToString());
+            if (target == BuildTarget.iOS) 
+            {
+                IOSPostBuildActions(target, pathToBuiltProject);
             }
+            else if (target == BuildTarget.Android) 
+            {
+                AndroidPostBuildActions(target, pathToBuiltProject);
+            }
+        }
+
+        private static void AndroidPostBuildActions(BuildTarget target, string pathToBuiltProject) 
+        {
+            
+        }
+        
+        private static void IOSPostBuildActions(BuildTarget target, string pathToBuiltProject) 
+        {
+            string plistPath = pathToBuiltProject + "/Info.plist";
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+       
+            PlistElementDict rootDict = plist.root;
+                
+            rootDict.SetBoolean ("ITSAppUsesNonExemptEncryption", false);
+       
+            File.WriteAllText(plistPath, plist.WriteToString());
         }
     }
 }
